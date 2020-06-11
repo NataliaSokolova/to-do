a634230824ea5eb492d9b7cdc0db253a908454e3
Natalia Sokolova <nataliya.a.sokolova@gmail.com>
2020-06-10 18:14:44 -0700

    added handleDelete, clearList, handleSubmit functionality

--- src/App.js
+++ src/App.js
@@ -49,7 +49,20 @@ this.setState({
       item.id !== id)
       this.setState({
         items:filteredItems
-      })
+      });
+  };
+  handleEdit = id =>{
+    const filteredItems = this.state.items.filter(item =>
+      item.id !== id);
+
+    const selectedItem = this.state.items.find(item=> item.id ==id)
+
+      this.setState({
+        items:filteredItems,
+        item: selectedItem.title,
+        editItem:true,
+        id:id,
+      });
   }
 
   render() {
@@ -65,10 +78,13 @@ this.setState({
                    item = {this.state.item} 
                    handleChange = {this.handleChange}
                    handleSubmit = {this.handleSubmit}
+                   editItem = {this.state.editItem}
                    />
-               <TodoList items = {this.state.items}
+               <TodoList 
+                   items = {this.state.items}
                    clearList = {this.clearList}
                    handleDelete = {this.handleDelete}
+                   handleEdit= {this.handleEdit}
                    />
 
             </div>
